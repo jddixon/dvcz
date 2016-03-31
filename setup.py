1f36ad8e5b0005952c34380f736d14e4da46143d
Jim Dixon <jddixon@gmail.com>
2016-03-31 09:49:00 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# dvcz/setup.py
+
+import re
+from distutils.core import setup
+__version__ = re.search("__version__\s*=\s*'(.*)'",
+                        open('dvcz/__init__.py').read()).group(1)
+
+# see http://docs.python.org/distutils/setupscript.html
+
+setup(name='dvcz',
+      version=__version__,
+      author='Jim Dixon',
+      author_email='jddixon@gmail.com',
+      py_modules=[],
+      packages=['dvcz'],
+      # following could be in scripts/ subdir
+      scripts=[],
+      # MISSING url
+      )
