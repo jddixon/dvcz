87f407f50bb4f50b27e92fb296d8a0ca6e1bcc40
Jim Dixon <jddixon@gmail.com>
2016-04-21 14:01:13 -0700

    add .projCfg, fix countem, edits to dvcCommit

--- dvcz/__init__.py
+++ dvcz/__init__.py
@@ -3,6 +3,6 @@
 __all__ = ['__version__', '__version_date__',
            ]
 
-__version__      = '0.0.2'
-__version_date__ = '2016-04-18'
+__version__      = '0.0.3'
+__version_date__ = '2016-04-21'
 
