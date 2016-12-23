afca75c38963cb4cee73e02ed063a89a74ee4fd5
Jim Dixon <jddixon@gmail.com>
2016-12-22 19:54:00 -0800

    add {,Pub}{User,Committer} and Project abstractions

--- dvcz/__init__.py
+++ dvcz/__init__.py
@@ -10,9 +10,11 @@ import hashlib
 
 from buildlist import(check_dirs_in_path, generate_rsa_key,
                       read_rsa_key, rm_f_dir_contents)
-from rnglib import FILE_NAME_STARTERS, FILE_NAME_CHARS
+from rnglib import valid_file_name
 from xlattice import QQQ
 from xlattice.u import UDir
+
+from Crypto.PublicKey import RSA
 
 if sys.version_info < (3, 6):
     import sha3
@@ -22,40 +24,8 @@ __all__ = ['__version__', '__version_date__',
            'do_add_user',
            'DvczError']
 
-__version__ = '0.0.20'
-__version_date__ = '2016-12-21'
-
-
-class DvczError(RuntimeError):
-    pass
-
-VALID_FILE_NAME_PAT = \
-    r'^[' + FILE_NAME_STARTERS + '](?:' + FILE_NAME_CHARS + ')*'
-VALID_FILE_NAME_RE = re.compile(VALID_FILE_NAME_PAT)
-
-
-def validFileName(string):
-    """
-    Return whether this is a valid file name for our purposes.
-
-    This means that the starter character is in the string of acceptable
-    starting characters and that any other characters are in the string
-    of acceptable 'other' characters.
-
-    XXX This function should be in a less specialized library.
-    """
-    if not string:
-        valid = False
-    else:
-        valid = True
-        if not string[0] in FILE_NAME_STARTERS:
-            valid = False
-        else:
-            for ndx in range(1, len(string)):
-                if not string[ndx] in FILE_NAME_CHARS:
-                    valid = False
-                    break
-    return valid
+__version__ = '0.1.0'
+__version_date__ = '2016-12-22'
 
 
 def get_proj_info(options):
@@ -156,7 +126,7 @@ def do_add_user(options):
     if not os.path.exists(options.user_dvcz_path):
         os.makedirs(options.user_dvcz_path, exist_ok=True, mode=0o755)
 
-    # write RSA private key to $DVCZ_DIR/node/skPriv.pem
+    # write RSA private key to $DVCZ_DIR/node/sk_priv.pem
     if not os.path.exists(options.key_path):
         check_dirs_in_path(options.key_path)
         if options.testing:
@@ -209,3 +179,197 @@ def do_add_user(options):
                                   os.path.join('in', committer_id))
         # my_in_dir =
         UDir.discover(my_in_path, using_sha=using_sha)
+
+# CLASSES ===========================================================
+
+
+class DvczError(RuntimeError):
+    pass
+
+
+class User(object):
+    """
+    This version of the user descriptor includes secret RSA keys.
+    """
+
+    def __init__(self, login, sk_priv=None, ck_priv=None, key_bits=2048):
+
+        # The login must always be a valid name, one including no
+        # delimiters or other odd characters.
+
+        if not valid_name(login):
+            raise AtttributeError("not a valid login: '%s'" % login)
+
+        if sk_priv is None:
+            sk_priv = RSA.generate(bit_count)
+        if ck_priv is None:
+            ck_priv = RSA.generate(bit_count)
+        # To write use
+        #   with open(path, 'wb+') as file: file.write(sk_priv.exportKey('PEM'))
+        # To read use
+        #   with open(path, 'rb') as file: sk_priv = RSA.importKey(file.read())
+        self._sk_priv = sk_priv
+        self._ck_priv = ck_priv
+
+    @property
+    def login(self):
+        return self._login
+
+    @property
+    def sk_priv(self):
+        return self._sk_priv
+
+    @property
+    def ck_priv(self):
+        return self._ck_priv
+
+    @property
+    def key_bits(self):
+        return self._key_bits
+
+    def __str__(self):
+
+        return "NOT IMPLEMENTED: User.__str__()"
+
+    def create_from_file(self, path):
+
+        return NotImplementedError()
+
+
+class PubUser(object):
+    """
+    This version of the user descriptor includes the public part of  RSA keys.
+    """
+
+    def __init__(self, login, sk, ck):
+
+        # The login must always be a valid name, one including no
+        # delimiters or other odd characters.
+
+        if not valid_name(login):
+            raise AtttributeError("not a valid login: '%s'" % login)
+
+        # To write use
+        #   with open(path, 'wb+') as file: file.write(sk.exportKey('PEM'))
+        # To read use
+        #   with open(path, 'rb') as file: sk = RSA.importKey(file.read())
+        self._sk = sk
+        self._ck = ck
+
+    @property
+    def login(self):
+        return self._login
+
+    @property
+    def sk(self):
+        return self._sk
+
+    @property
+    def ck(self):
+        return self._ck
+
+    def __str__(self):
+
+        return "NOT IMPLEMENTED: User.__str__()"
+
+    def create_from_file(self, path):
+
+        raise NotImplementedError()
+
+    @classmethod
+    def create_from_user(cls, user):
+        """ Replaces private RSA keys with public parts. """
+        return PubUser(user.login,
+                       user.sk_priv.publickey(),
+                       user.ck_priv.publickey())
+
+
+class Committer(User):
+    """
+    """
+
+    def __init__(self, login, handle,
+                 sk_priv=None, ck_priv=None, key_bits=2048):
+        super().__init(login, sk_priv, ck_priv, key_bits)
+
+
+class PubCommitter(PubUser):
+    """
+    """
+
+    def __init__(self, login, handle,
+                 sk_priv=None, ck_priv=None, key_bits=2048):
+        super().__init(login, sk_priv, ck_priv, key_bits)
+
+
+class DvcProject(object):
+    """
+    Dvcz project descriptor.
+
+    The project name must be a valid name (incorporate no delimiters or
+    odd characters) and should be unique in the context.  The project
+    path (proj_path) may be either relative or absolute.  If it does
+    not exist it will be created with the mode indicated.  main_lang,
+    indicating the computer language used, is optional but if supplied
+    must be another valid name.
+    """
+
+    def __init__(self, name, proj_path, main_lang='', mode=0o755):
+
+        if not valid_file_name(name):
+            raise AtttributeError("not a valid project name: '%s'" % name)
+        if main_lang and not valid_file_name(main_lang):
+            raise AtttributeError(
+                "not a valid language name: '%s'" %
+                main_lang)
+
+        os.makedirs(proj_path, exist_ok=True, mode=mode)
+
+        self._name = name
+        self._proj_path = proj_path
+        self._main_lang = main_lang
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def proj_path(self):
+        return self._proj_path
+
+    @property
+    def main_lang(self):
+        return self._main_lang
+
+
+class Store(UDir):
+    """
+    Specifies a content-keyed store.
+
+    The name should be unique within the context and must be a valid file
+    name.  The name need have nothing to do with u_path.
+
+    If the directory at u_path already exists, its directory
+    structure (dir_struc) and SHA hash type (using_sha) are discovered
+    and override the attributes supplied.
+
+    If u_path does not exist, the directory is created using the attributes
+    passed.
+
+    """
+
+    def __init__(self, name, u_path, dir_struc=UDir.DIR_FLAT,
+                 using_sha=QQQ.USING_SHA2, mode=0o755):
+
+        if not valid_file_name(name):
+            raise AttributeError("not a valid file name: '%s'" % name)
+
+        super().__init__(u_path, dir_struc, using_Sha, mode)
+        self._name = name
+
+    @property
+    def name(self):
+        """
+        Return the name assigned to the store.  This is a valid file
+        name, a single word incorporating no delimiters.
+        """
--- test_dvc_setup.py
+++ test_dvc_setup.py
@@ -9,80 +9,127 @@ import unittest
 
 from dvcz import get_proj_info, DvczError
 from rnglib import SimpleRNG, valid_file_name
+from xlattice.u import UDir
+
+
+class DvcTestSetup(object):
+    """
+    Create a directory structure under ./tmp/
+
+    This looks like
+        tmp/
+          RUN_ID/
+            home/
+              LOGIN/
+                dvcz/
+                  committers/
+                    HANDLE      # serialized Committer object
+                    ...         #   containing secret keys, etc
+                  stores/
+                    STORE_NAME  # serialized UStore object
+                    ...
+            projects/
+              PROJECT_NAME/
+                dvcz/
+              ...
+            stores/
+              STORE_NAME/
+                SHA_SIG     # hex value of SHAxNONE, where x is 1, 2, or 3
+                in/
+                  USER_ID
+                    SHA_SIG
+                    in/
+                    tmp/
+                    CONTENT_KEY
+                    ...
+
+    That is, each run ID has a distinct subdirectory under tmp.
+    In each such subdirectory there is a dummy home/login directory,
+    and under that a dvcz directory that corresponds to $HOME/.dvcz.
+    The keys in this dummy dvcz directory are used for digital
+    signatures on BuildLists created during test runs
+
+    At the same level as the home/ directory there is a projects/
+    directory and under that dummy project directories for the test
+    runs.  At the time of writing these are used only for collecting
+    lastBuildList files and appending build info to the builds file.
+
+    And at the same level there are dummy content-keyed stores used
+    in the test runs.  Each such store has a unique name.  Each has
+    an in/ directory and within that
+    """
+
+    def __init__(self):
+
+        self._rng = SimpleRNG()
+        self._run_id = self.rng.next_file_name(8)
+        self._run_dir = os.path.join('tmp', self._run_id)
+        while os.path.exists(self._run_dir):
+            self._run_id = self.rng.next_file_name(8)
+            self._run_dir = os.path.join('tmp', self._run_id)
+        os.makedirs(self._run_dir, mode=0o755)
+
+        # under this add home/LOGIN/dvcz/home_committers/ and stores/
+        self._login = os.environ['LOGNAME']
+        dvc_dir = os.path.join(
+            self._run_dir, os.path.join(
+                'home', os.path.join(self._login, 'dvcz')))
+        self._home_committers_dir = os.path.join(dvc_dir, 'home_committers')
+        self._home_stores_dir = os.path.join(dvc_dir, 'stores')
+        os.makedirs(self._home_committers_dir, mode=0o755)
+        os.makedirs(self._home_stores_dir, mode=0o755)
+
+        # on the same level as home
+        self._projects_dir = os.path.join(self._run_dir, 'projects')
+        os.makedirs(self._projects_dir, mode=0o755)
+
+        # on the same level as home
+        self._stores_dir = os.path.join(self._run_dir, 'stores')
+        os.makedirs(self._stores_dir, mode=0o755)
+
+        # maps
+        self._name2project = None
+        self._name2store = None
+
+        # lists
+        self._projects = []     # a list of Project objects
+        self._stores = []       # a list of UStore objects
+
+    @property
+    def rng(self):
+        return self._rng
+
+    @property
+    def run_id(self):
+        return self._run_id
+
+    @property
+    def run_dir(self):
+        return self._run_dir
+
+    @property
+    def login(self):
+        return self._login
+
+    @property
+    def home_committers_dir(self):
+        return self._home_committers_dir
+
+    @property
+    def home_stores_dir(self):
+        return self._home_stores_dir
+
+    @property
+    def projects_dir(self):
+        return self._projects_dir
+
+    @property
+    def stores_dir(self):
+        return self._stores_dir
 
 
 class TestDvcSetup(unittest.TestCase):
     """ Test the setUp function for dvcz testing. """
-
-    login, run_id, run_dir = '', '', ''
-    home_committers_dir, home_stores_dir = '', ''
-    rng = SimpleRNG()
-
-    def setUp(self):
-        """
-        Create a directory structure under tmp/
-
-        This looks like
-            tmp/
-              RUN_ID/
-                home/
-                  LOGIN/
-                    dvcz/
-                      committers/
-                        HANDLE      # serialized Committer object
-                        ...         #   containing secret keys, etc
-                      stores/
-                        STORE_NAME  # serialized UStore object
-                        ...
-                projects/
-                  PROJECT_NAME/
-                    dvcz/
-                  ...
-                stores/
-                  STORE_NAME/
-                    SHA_SIG     # hex value of SHAxNONE, where x is 1, 2, or 3
-                    in/
-                      USER_ID
-                        SHA_SIG
-                        in/
-                        tmp/
-                        CONTENT_KEY
-                        ...
-
-        That is, each run ID has a distinct subdirectory under tmp.
-        In each such subdirectory there is a dummy home/login directory,
-        and under that a dvcz directory that corresponds to $HOME/.dvcz.
-        The keys in this dummy dvcz directory are used for digital
-        signatures on BuildLists created during test runs
-
-        At the same level as the home/ directory there is a projects/
-        directory and under that dummy project directories for the test
-        runs.  At the time of writing these are used only for collecting
-        lastBuildList files and appending build info to the builds file.
-
-        And at the same level there are dummy content-keyed stores used
-        in the test runs.  Each such store has a unique name.  Each has
-        an in/ directory and within that
-        """
-
-        # tmp/self.run_id
-
-        self.run_id = self.rng.next_file_name(8)
-        self.run_dir = os.path.join('tmp', self.run_id)
-        while os.path.exists(self.run_dir):
-            self.run_id = self.rng.next_file_name(8)
-            self.run_dir = os.path.join('tmp', self.run_id)
-        os.makedirs(self.run_dir, mode=0o755)
-
-        # under this add home/LOGIN/dvcz/home_committers/ and stores/
-        self.login = os.environ['LOGNAME']
-        dvc_dir = os.path.join(
-            self.run_dir, os.path.join(
-                'home', os.path.join(self.login, 'dvcz')))
-        self.home_committers_dir = os.path.join(dvc_dir, 'home_committers')
-        self.home_stores_dir = os.path.join(dvc_dir, 'stores')
-        os.makedirs(self.home_committers_dir, mode=0o755)
-        os.makedirs(self.home_stores_dir, mode=0o755)
 
     def tearDown(self):
         pass
@@ -92,24 +139,31 @@ class TestDvcSetup(unittest.TestCase):
         We verify that setUp() has created the expected directory
         structure.
         """
-        self.assertTrue(valid_file_name(self.run_id))
-        self.assertTrue(os.path.exists(self.run_dir))
-        self.assertEqual(self.run_dir, os.path.join('tmp', self.run_id))
 
-        # weird problem with python
+        cfg = DvcTestSetup()
+
+        self.assertTrue(valid_file_name(cfg.run_id))
+        self.assertTrue(os.path.exists(cfg.run_dir))
+        self.assertEqual(cfg.run_dir, os.path.join('tmp', cfg.run_id))
+
+        # weird problem with python: os.getlogin() usually returns '' but
+        # sometimes returns eg 'jdd'
         try:
             ret = os.getlogin()
             print("os.getlogin() unexpecedly worked, returning %s" % ret)
         except FileNotFoundError:
             pass
 
-        self.assertEqual(self.login, os.environ['LOGNAME'])
+        self.assertEqual(cfg.login, os.environ['LOGNAME'])
 
         # pure lazinessM
-        self.assertEqual(self.home_committers_dir, self.run_dir + '/home/' +
-                         self.login + '/dvcz/home_committers')
-        self.assertTrue(os.path.exists(self.home_committers_dir))
-        self.assertTrue(os.path.exists(self.home_stores_dir))
+        self.assertEqual(cfg.home_committers_dir, cfg.run_dir + '/home/' +
+                         cfg.login + '/dvcz/home_committers')
+        self.assertTrue(os.path.exists(cfg.home_committers_dir))
+        self.assertTrue(os.path.exists(cfg.home_stores_dir))
+
+        self.assertTrue(os.path.exists(cfg.projects_dir))
+        self.assertTrue(os.path.exists(cfg.stores_dir))
 
 if __name__ == '__main__':
     unittest.main()
